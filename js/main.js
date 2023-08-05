ea5e042bd09a8592ec0c8dac7a63faa84106e10f
arvdevable <106006082+arvdevable@users.noreply.github.com>
2023-08-05 14:44:45 +0700

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,30 @@
+function updateTime() {
+    const now = new Date();
+    const currentTime = now.toLocaleTimeString('en-US', { hour12: false });
+    document.getElementById('current-time').textContent = `${currentTime}`;
+}
+
+function updateDate() {
+    const now = new Date();
+    const currentDate = now.toLocaleDateString('en-US');
+    document.getElementById('current-date').textContent = `${currentDate}`;
+}
+
+// Call initial functions and update every second
+setInterval(() => {
+    updateDate();
+    updateTime();
+}, 1000);
+
+function tz() {
+    const timezone = Intl.DateTimeFormat().resolvedOptions().timeZone;
+    const now = new Date();
+    const offset = now.getTimezoneOffset();
+    const offsetHours = Math.abs(Math.floor(offset / 60));
+    const offsetMinutes = Math.abs(offset % 60);
+    const offsetString = (offset >= 0 ? '-' : '+') + offsetHours.toString().padStart(2, '0') + ':' + offsetMinutes.toString().padStart(2, '0');
+    
+    document.getElementById('tz').textContent = `${timezone} (UTC${offsetString})`;
+}
+
+tz();
